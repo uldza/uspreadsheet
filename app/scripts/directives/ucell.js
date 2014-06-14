44fa9cbc21bd8362067119cf96281f0760fc0a12
Uldis Megnis <uldis.megnis@gmail.com>
2014-06-15 00:04:25 +0300

    Fight data bindings

--- app/scripts/directives/ucell.js
+++ app/scripts/directives/ucell.js
@@ -5,14 +5,12 @@ angular.module('uSpreadsheetApp')
         return {
             restrict: 'A',
             controller: 'CellCtrl',
-            template: '<span>{{cellIndex}}</span>',
-            scope: {
-                cellIndex: '@uCell'    
-            },
             link: function ($scope, element, attrs, Ctrl) {
+                Ctrl.index = $scope.colLetter($scope.col)+$scope.row;
+                
+                element.text(Ctrl.index);
                 element.on('click', function(event) {
                     event.preventDefault();
-                    alert($scope.cellIndex);
                 });
             }
         }
