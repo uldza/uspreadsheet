64d79d531701f86bf38ee4dc65fac506c15064e7
Uldzh <uldis.megnis@gmail.com>
2014-06-22 16:27:17 +0300

    Move cell-input css styles from directive to main.css

--- app/scripts/directives/cellInput.js
+++ app/scripts/directives/cellInput.js
@@ -7,36 +7,9 @@ angular.module('uSpreadsheetApp')
             controller: 'CellInputCtrl',
             template: '<input type="text" class="cell-input" data-ng-model="value" />',
             link: function ($scope, element, attrs, Ctrl) {
+                element.addClass('cell-input-box');
+
                 Ctrl.init(element);
-                
-                element.css('position', 'absolute');
-                element.css('top', '-1000px');
-                element.css('border', '2px #5292f7 solid');
-                element.css('padding', '0 2px');
-                element.css('margin', '0'); 
-                element.css('z-index', '15'); 
-                element.css('resize', 'none'); 
-                element.css('overflow', 'auto'); 
-                element.css('white-space', 'pre-wrap'); 
-                element.css('outline', 'none'); 
-                element.css('-webkit-box-shadow', '0 2px 5px rgba(0,0,0,0.4)'); 
-                element.css('-moz-box-shadow', '0 2px 5px rgba(0,0,0,0.4)');
-                element.css('box-shadow', '0 2px 5px rgba(0,0,0,0.4)');
-                element.css('word-wrap', 'break-word');
-                element.css('background', 'white');
-
-                element.find('.cell-input').css('padding', '0 2px');
-
-                // Changable styles
-                element.css('font', 'normal normal 400 13px arial,sans,sans-serif');
-                element.css('text-align', 'left');
-
-                element.find('.cell-input').css('max-width', '592px');
-                element.find('.cell-input').css('max-height', '80px');
-                element.find('.cell-input').css('min-width', '114px');
-                element.find('.cell-input').css('min-height', '14px');
-                element.find('.cell-input').css('width', '100%');
-                element.find('.cell-input').css('border', '0px');
             }
         };
     });
